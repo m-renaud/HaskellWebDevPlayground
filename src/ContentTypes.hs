ce37e85c5638fad1af9952b3c3b7bd694180a54b
Matt Renaud <mrenaud92@gmail.com>
2017-07-12 23:02:13 -0700

    Replace homegrown HTML content type with Servant.HTML.Lucid.HTML

--- src/ContentTypes.hs
+++ src/ContentTypes.hs
@@ -10,7 +10,7 @@ associated instance in scope within Lib.hs. The corresponding instances are:
 
     ContentType     Instance
     --------------------------
-    HTML            Lucid.ToHtml
+    Servant.HTML    Lucid.ToHtml
     Servart.JSON    Data.Aeson.ToJSON (usually done with deriveJSON TH)
 
 To add a new content type:
@@ -19,23 +19,9 @@ To add a new content type:
      typeclasses
 -}
 module ContentTypes
-    ( HTML
+    ( Servant.HTML.Lucid.HTML
     , Servant.JSON
     ) where
 
-import Lucid (Html, ToHtml(..), toHtml, renderBS)
-import Network.HTTP.Media ((//), (/:))
-import Servant (Accept(..), MimeRender(..))
+import qualified Servant.HTML.Lucid
 import qualified Servant
-
-
-data HTML
-
-instance Accept HTML where
-    contentType _ = "text" // "html" /: ("charset", "utf-8")
-
-instance ToHtml a => MimeRender HTML a where
-    mimeRender _ = renderBS . toHtml
-
-instance MimeRender HTML (Html a) where
-    mimeRender _ = renderBS
